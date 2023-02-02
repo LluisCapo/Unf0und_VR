c7fe3fb9fb05c88f7e4b182789ef7f3754bbe620
xtrexxGit <116378394+LluisCapo@users.noreply.github.com>
2023-02-02 16:07:10 +0100

    created

---
+++ Unf0und_VR/Assets/Lluis/Scripts/BBallInstantiate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BBallInstantiate : MonoBehaviour
+{
+    // 02/02/2023 --> Lluís Capó
+
+    private void Start()
+    {
+        
+    }
+
+}
