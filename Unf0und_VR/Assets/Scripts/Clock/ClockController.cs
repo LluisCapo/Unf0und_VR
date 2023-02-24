93267f14a764b209b5a1b509347dc1fb8d8302b4
Aaron_chicano <92025194+Arboy23@users.noreply.github.com>
2023-02-24 19:56:43 +0100

    clock p

--- Unf0und_VR/Assets/Scripts/Clock/ClockController.cs
+++ Unf0und_VR/Assets/Scripts/Clock/ClockController.cs
@@ -6,24 +6,43 @@ public class ClockController : MonoBehaviour
 {
     [SerializeField] GameObject canvas;
     [SerializeField] float secondsToInteract;
+    [SerializeField] BoxCollider boxCollider;
     float _delay;
     private void Start()
     {
         _delay = .0f;
+        gameObject.layer = 30;
     }
+
     private void OnTriggerStay(Collider other)
     {
         if (!canvas.activeInHierarchy)
         {
             if ((_delay += Time.deltaTime) >= secondsToInteract)
-                Debug.Log("sale canvas"); //canvas.SetActive(true);
+            {
+                Debug.Log("entra canvas"); //canvas.SetActive(true);
+                StartCoroutine(HandClock());
+                _delay = .0f;
+            }
+
         }
         else
-            canvas.SetActive(false);
+        {
+            //canvas.SetActive(false);
+            _delay = .0f;
+        }
+            
 
+    }
+    private IEnumerator HandClock()
+    {
+        boxCollider.enabled = false;
+        yield return new WaitForSeconds(4f);
+        boxCollider.enabled = true;
     }
     private void OnTriggerExit(Collider other)
     {
+        Debug.Log("sale canvas");
         _delay = 0;
     }
 }
