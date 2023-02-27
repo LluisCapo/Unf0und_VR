ada3bc4b56c83f41dd5a8139724506a8a851ec04
Aaron_chicano <92025194+Arboy23@users.noreply.github.com>
2023-02-27 16:30:21 +0100

    27/02 16:30

--- Unf0und_VR/Assets/Scripts/Clock/ClockController.cs
+++ Unf0und_VR/Assets/Scripts/Clock/ClockController.cs
@@ -7,42 +7,46 @@ public class ClockController : MonoBehaviour
     [SerializeField] GameObject canvas;
     [SerializeField] float secondsToInteract;
     [SerializeField] BoxCollider boxCollider;
-    float _delay;
+    bool activeInvoke = false;
+
+
+
     private void Start()
     {
-        _delay = .0f;
         gameObject.layer = 30;
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (!canvas.activeInHierarchy)
-        {
-            if ((_delay += Time.deltaTime) >= secondsToInteract)
-            {
-                Debug.Log("entra canvas"); //canvas.SetActive(true);
-                StartCoroutine(HandClock());
-                _delay = .0f;
-            }
-
-        }
-        else
-        {
-            //canvas.SetActive(false);
-            _delay = .0f;
-        }
-            
-
-    }
-    private IEnumerator HandClock()
-    {
-        boxCollider.enabled = false;
-        yield return new WaitForSeconds(4f);
-        boxCollider.enabled = true;
-    }
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("sale canvas");
-        _delay = 0;
+        Invoke("OpenCanvas", 0);
+    }
+
+    private void OpenCanvas()
+    {
+        //if (!canvas.activeInHierarchy)
+        //{
+        //    Debug.Log("entra canvas"); //canvas.SetActive(true);
+        //    StartCoroutine(HandClock());
+        //    _delay = .0f;
+
+        //}
+        //else
+        //{
+        //    //canvas.SetActive(false);
+        //    _delay = .0f;
+        //}
+        if(!activeInvoke)
+        {
+            Invoke("ActiveCanvas", 1);
+            activeInvoke = true;
+        }
+            
+            
+    }
+
+    private void ActiveCanvas()
+    {
+        canvas.SetActive(!canvas.activeInHierarchy);
+        activeInvoke= false;
     }
 }
