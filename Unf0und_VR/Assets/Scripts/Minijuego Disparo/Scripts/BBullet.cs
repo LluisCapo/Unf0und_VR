cefd5526c19c218ef88d8c917ddc34be397913fd
RaulCano <91997608+calaveiraUwU@users.noreply.github.com>
2023-03-03 19:40:35 +0100

    c

--- Unf0und_VR/Assets/Scripts/Minijuego Disparo/Scripts/BBullet.cs
+++ Unf0und_VR/Assets/Scripts/Minijuego Disparo/Scripts/BBullet.cs
@@ -8,6 +8,7 @@ public class BBullet : MonoBehaviour
     float speed;
 
     private Rigidbody _rb;
+    private GameObject hit;
 
 
     public float Speed { get { return speed; } set { speed = value; } }
@@ -24,9 +25,15 @@ public class BBullet : MonoBehaviour
         _rb.velocity = new Vector3(0, 0, 0);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(other.name);
+        //)
+        //Vector3 puntocolision = ClosestPointOnBounds(transform.position);//other.ClosestPoint(transform.position);
+        hit = PoolingManager.Instance.GetPooledObject("BulletMark");
+        hit.transform.position = collision.contacts[0].point; // puntocolision;//  new Vector3(transform.position.x, transform.position.y, transform.position.z - other.GetComponent<BoxCollider>().transform.position.z);
+        hit.transform.localScale = transform.localScale / 6;//new Vector3(transform.localScale.x, transform.localScale.y, hit.transform.localScale.z);
+        hit.transform.rotation = new Quaternion(hit.transform.rotation.x - collision.transform.rotation.x, collision.transform.rotation.y, collision.transform.rotation.z, hit.transform.rotation.w);
+        //;
         gameObject.SetActive(false);
     }
 
