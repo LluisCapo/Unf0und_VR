dadd1266cf77dd7ce04560315fe2c73249394760
Aaron_chicano <92025194+Arboy23@users.noreply.github.com>
2023-05-14 14:12:56 +0200

    A

--- Unf0und_VR/Assets/Scenes/Scripts/General/DrawMesh/DrawMeshObjectsCine.cs
+++ Unf0und_VR/Assets/Scenes/Scripts/General/DrawMesh/DrawMeshObjectsCine.cs
@@ -14,7 +14,7 @@ public class DrawMeshObjectsCine : MonoBehaviour
     private void Update()
     {
         for (float i = 0; i <= (separacionY * obj); i += separacionY)
-            Graphics.DrawMesh(mesh, new Vector3(transform.position.x, transform.position.y + (i/5), transform.position.z - i),Quaternion.Euler(-90, gameObject.transform.localRotation.y,0),material, layer);
+            Graphics.DrawMesh(mesh, new Vector3(transform.position.x, transform.position.y + (i/5), transform.position.z - i),Quaternion.Euler(-90, gameObject.transform.localRotation.eulerAngles.y-180,0),material, layer);
 
 
 
